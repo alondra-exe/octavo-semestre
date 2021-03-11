4360447f27755bece5d85cc7adbae7eba2c26a28
Alondra Delgadillo <171G0576@rcarbonifera.tecnm.mx>
2021-03-11 14:10:08 -0600

    Proyecto unidad 1.

---
+++ programacion-movil/unidad-1/pokedex/pokedex/models/viewModels/pokedexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+using pokedex.models;
+
+namespace pokedex.models.viewModels
+{
+    public class pokedexViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Pokemon> Pokemons { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- programacion-movil/unidad-1/pokemon-api/Controllers/PokedexController.cs
+++ programacion-movil/unidad-1/pokemon-api/Controllers/PokedexController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using pokemon_api.models;
+using pokemon_api.repositories;
 
 namespace pokemon_api.controllers
 {
@@ -11,17 +13,40 @@ namespace pokemon_api.controllers
     [ApiController]
     public class pokedexController : ControllerBase
     {
-        // Regresa el número, nombre, tipo e imagen del pokémon.
-        [HttpGet]
-        public IActionResult GetPokemonList(int id)
+        public sistem14_pokemonContext Context { get; set; }
+        public pokedexController(sistem14_pokemonContext context)
         {
-            return Ok();
+            Context = context;
         }
 
-        [HttpGet]
+        // Regresa el número, nombre, tipo e imagen del pokémon.
+        [HttpGet("lista/{id}")]
+        public IActionResult GetPokemonList(int id)
+        {
+            pokemonRepository repos = new pokemonRepository(Context);
+            var datos = repos.GetList(id).Select(x => new Pokemon
+            {
+                Id = x.Id,
+                Nombre = x.Nombre,
+                Tipo1 = x.Tipo1,
+                Tipo2 = x.Tipo2,
+                Imagen1 = x.Imagen1
+            }).ToList();
+            if (datos != null)
+                return Ok(datos);
+            else
+                return NotFound();
+        }
+
+        [HttpGet("datos/{id}")]
         public IActionResult GetPokemonData(int id)
         {
-            return Ok();
+            pokemonRepository repos = new pokemonRepository(Context);
+            var datos = repos.GetPokemon(id);
+            if (datos != null)
+                return Ok(datos);
+            else
+                return NotFound();
         }
     }
 }
