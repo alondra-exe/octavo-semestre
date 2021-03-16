2744a5c8a374fd620062f920886b0c7178308bd4
Alondra Delgadillo <171G0576@rcarbonifera.tecnm.mx>
2021-03-16 15:33:43 -0600

    Repositorio base.

---
+++ programacion-movil/unidad-1/proyecto/proyecto1-api/Controllers/DocentesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyecto1_api.Models;
+
+namespace proyecto1_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocentesController : ControllerBase
+    {
+        private readonly sistem14_proyecto1_alondra_jesmeContext _context;
+
+        public DocentesController(sistem14_proyecto1_alondra_jesmeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Docentes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Docente>>> GetDocente()
+        {
+            return await _context.Docente.ToListAsync();
+        }
+
+        // GET: api/Docentes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Docente>> GetDocente(int id)
+        {
+            var docente = await _context.Docente.FindAsync(id);
+
+            if (docente == null)
+            {
+                return NotFound();
+            }
+
+            return docente;
+        }
+
+        // PUT: api/Docentes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDocente(int id, Docente docente)
+        {
+            if (id != docente.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(docente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DocenteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Docentes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Docente>> PostDocente(Docente docente)
+        {
+            _context.Docente.Add(docente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDocente", new { id = docente.Id }, docente);
+        }
+
+        // DELETE: api/Docentes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Docente>> DeleteDocente(int id)
+        {
+            var docente = await _context.Docente.FindAsync(id);
+            if (docente == null)
+            {
+                return NotFound();
+            }
+
+            _context.Docente.Remove(docente);
+            await _context.SaveChangesAsync();
+
+            return docente;
+        }
+
+        private bool DocenteExists(int id)
+        {
+            return _context.Docente.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ programacion-movil/unidad-1/proyecto/proyecto1-api/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proyecto1_api.Repositories
+{
+    public abstract class Repository<T> where T : class
+    {
+        public DbContext Context { get; set; }
+        public virtual T Get(object id)
+        {
+            return Context.Find<T>(id);
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>();
+        }
+
+        public virtual void Insert(T entity)
+        {
+            Context.Add(entity);
+            Context.SaveChanges();
+        }
+
+        public virtual void Update(T entity)
+        {
+            Context.Update(entity);
+            Context.SaveChanges();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            Context.Remove(entity);
+            Context.SaveChanges();
+        }
+
+        public virtual bool IsValid(T entity, out List<string> errors)
+        {
+            errors = new List<string>();
+            return true;
+        }
+    }
+}
