a3a16dedef5972825a662c288546b2bb697634f8
EsmeraldaGP <eessme.padilla@hotmail.com>
2021-03-19 01:35:47 -0600

    Avance InicioSesion y vistas

--- programacion-movil/unidad-1/proyecto/proyecto1-web/Controllers/AlumnosController.cs
+++ programacion-movil/unidad-1/proyecto/proyecto1-web/Controllers/AlumnosController.cs
@@ -19,7 +19,7 @@ namespace proyecto1_web.Controllers
         public async Task<IActionResult> Index()
         {
             HttpClient cliente = Factory.CreateClient("proyecto1-api");
-            var result = await cliente.GetAsync("alumno");
+            var result = await cliente.GetAsync("alumnos");
 
             if (result.StatusCode == System.Net.HttpStatusCode.OK)
             {
@@ -43,8 +43,6 @@ namespace proyecto1_web.Controllers
         public async Task<IActionResult> AgregarAlumno(Models.Alumno alumno)
         {
             HttpClient cliente = Factory.CreateClient("proyecto1-api");
-
-            //Validado
 
             var json = JsonConvert.SerializeObject(alumno);
             var result = await cliente.PostAsync("alumnos", new StringContent(json, System.Text.Encoding.UTF8, "application/json"));
@@ -97,7 +95,7 @@ namespace proyecto1_web.Controllers
             HttpClient cliente = Factory.CreateClient("proyecto1-api");
 
             var json = JsonConvert.SerializeObject(alumno);
-            var result = await cliente.PutAsync("alumno", new StringContent(json, System.Text.Encoding.UTF8, "application/json"));
+            var result = await cliente.PutAsync("alumnos", new StringContent(json, System.Text.Encoding.UTF8, "application/json"));
 
             if (result.IsSuccessStatusCode)
             {
@@ -118,5 +116,53 @@ namespace proyecto1_web.Controllers
                 return View(alumno);
             }
         }
+         
+        public async Task<IActionResult> VerProgreso()
+        {
+            HttpClient cliente = Factory.CreateClient("proyecto1-api");
+            var result = await cliente.GetAsync("progreso");
+
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var json = await result.Content.ReadAsStringAsync();
+                var datos = JsonConvert.DeserializeObject<List<Models.Progreso>>(json);
+                return View(datos);
+            }
+            else
+            {
+                ModelState.AddModelError("", "No se puede conectar con el servidor de datos");
+                return View(new List<Models.Progreso>());
+            }
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> EliminarAlumno(Alumno alumno)
+        //{
+        //    HttpClient cliente = Factory.CreateClient("proyecto1-api");
+           
+        //    var json = JsonConvert.SerializeObject(alumno);
+        //    var result = await cliente.DeleteAsync("alumno");
+
+        //    if (result.IsSuccessStatusCode)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    else if (result.StatusCode == System.Net.HttpStatusCode.BadRequest)
+        //    {
+        //        var jsonerrores = await result.Content.ReadAsStringAsync();
+        //        var lista = JsonConvert.DeserializeObject<List<string>>(jsonerrores);
+
+        //        lista.ForEach(x => ModelState.AddModelError("", x));
+
+        //        return View(alumno);
+        //    }
+        //    else
+        //    {
+        //        ModelState.AddModelError("", result.StatusCode.ToString());
+        //        return View(alumno);
+        //    }
+        //}
+
     }
 }
--- programacion-movil/unidad-1/proyecto/proyecto1-web/Controllers/HomeController.cs
+++ programacion-movil/unidad-1/proyecto/proyecto1-web/Controllers/HomeController.cs
@@ -1,63 +1,90 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using proyecto1_web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace proyecto1_web.Controllers
 {
     public class HomeController : Controller
     {
+        public IHttpClientFactory Factory { get; }
+        public HomeController(IHttpClientFactory factory)
+        {
+            Factory = factory;
+        }
+
+        [Authorize(Roles = "Docente")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult InicioSesionDocente()
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult InicioSesion()
         {
             return View();
         }
 
-        //public async Task<IActionResult> InicioSesionDocente(Docente d)
-        //{
-            
-        //    try
-        //    {
-                
-        //    }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> InicioSesion(Docente d)
+        {
+            HttpClient client = Factory.CreateClient("proyecto1-api");
+            d.Correo = d.Correo.ToLower();
+            var contra = Encoding.UTF8.GetBytes(d.Contrasena);
+            d.Contrasena = Convert.ToBase64String(contra);
 
-        //    catch (Exception ex)
-        //    {
-        //        ModelState.AddModelError("", ex.Message);
-        //        return View(d);
-        //    }
+            var json = JsonConvert.SerializeObject(d);
+            var result = await client.PostAsync("docente/iniciosesion", new StringContent(json, Encoding.UTF8, "application/json"));
 
-            //GymContext context = new GymContext();
-            //Repository<Administrador> repository = new Repository<Administrador>(context);
-            //var administrador = context.Administrador.FirstOrDefault(x => x.Clave == admin.Clave);
-            //try
-            //{
-            //    if (administrador != null && administrador.Contrasena == HashHelper.GetHash(admin.Contrasena))
-            //    {
-            //        List<Claim> info = new List<Claim>();
-            //        info.Add(new Claim(ClaimTypes.Name, "Usuario" + administrador.Nombre));
-            //        info.Add(new Claim(ClaimTypes.Role, "Administrador"));
-            //        info.Add(new Claim("clave", administrador.Nombre.ToString()));
-            //        info.Add(new Claim("Nombre", administrador.Nombre));
+            try
+            {
 
-            //        var claimsidentity = new ClaimsIdentity(info, CookieAuthenticationDefaults.AuthenticationScheme);
-            //        var claimsprincipal = new ClaimsPrincipal(claimsidentity);
-            //        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsprincipal,
-            //            new AuthenticationProperties { IsPersistent = true });
-            //        return RedirectToAction("IndexConfiguracion");
-            //    }
-            //    else
-            //    {
-            //        ModelState.AddModelError("", "La clave o la contraseña del administrador son incorrectas.");
-            //        return View(admin);
-            //    }
-            
-       // }
+                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim(ClaimTypes.Role, "Docente"));
+                    claims.Add(new Claim("Correo electronico", d.Correo));
+
+                    var claimidentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var claimprincipal = new ClaimsPrincipal(claimidentity);
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimprincipal,
+                    new AuthenticationProperties { IsPersistent = true });
+
+                    return RedirectToAction("ListaAlumnos");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("", "La clave o la contraseña del docente son incorrectas.");
+                    return View(d);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(d);
+            }
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> CerrarSesion()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
