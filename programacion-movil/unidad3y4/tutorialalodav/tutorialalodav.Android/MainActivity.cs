3cb088480232fa7b549f79f81ecb0e9f66d5a921
171G0247 <171G0247@rcarbonifera.tecnm.mx>
2021-06-18 16:53:11 -0500

    Banner

--- programacion-movil/unidad3y4/tutorialalodav/tutorialalodav.Android/MainActivity.cs
+++ programacion-movil/unidad3y4/tutorialalodav/tutorialalodav.Android/MainActivity.cs
@@ -17,12 +17,12 @@ namespace tutorialalodav.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+            MobileAds.Initialize(ApplicationContext);
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            MobileAds.Initialize(ApplicationContext);
+            
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
