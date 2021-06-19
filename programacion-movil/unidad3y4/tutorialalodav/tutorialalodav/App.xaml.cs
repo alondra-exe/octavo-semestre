6300a82dbce8fd4b6c1345ef1d7ee7bd4b8cf7a1
Alondra Delgadillo <171G0576@rcarbonifera.tecnm.mx>
2021-06-18 19:18:58 -0500

    Cambios.

--- programacion-movil/unidad3y4/tutorialalodav/tutorialalodav/App.xaml.cs
+++ programacion-movil/unidad3y4/tutorialalodav/tutorialalodav/App.xaml.cs
@@ -10,7 +10,7 @@ namespace tutorialalodav
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.Rewards());
+            MainPage = new NavigationPage(new Views.Banner());
         }
 
         protected override void OnStart()
